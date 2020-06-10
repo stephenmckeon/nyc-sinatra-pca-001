acb6e8821386689ee32831b83103593f09f7b816
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-06-10 11:15:08 -0400

    Pass tests

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,57 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get "/figures/new" do
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :"/figures/new"
+  end
+
+  post "/figures" do
+    figure = Figure.create(params[:figure])
+
+    if !params[:title][:name].empty?
+      figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    redirect "/figures/#{figure.id}"
+  end
+
+  get "/figures" do
+    @figures = Figure.all
+
+    erb :"/figures/index"
+  end
+
+  get "/figures/:id" do
+    @figure = Figure.find(params[:id])
+
+    erb :"/figures/show"
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :"/figures/edit"
+  end
+
+  patch "/figures/:id" do
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    redirect "/figures/#{figure.id}"
+  end
 end
